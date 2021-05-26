f4f9121d8f373a925e8de6192a0cd8a38faf2962
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-26 20:36:04 +0900

    added some components

--- src/Container/DrumMachine.js
+++ src/Container/DrumMachine.js
@@ -1,8 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function DrumMachine() {
   // Display Key Press Sate
   const [display, setDisplay] = useState('Press any key');
+
+  const audioArray = [
+    {
+      label: 'Heater 1',
+      keyCode: 81,
+      key: 'Q',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    },
+    {
+      label: 'Heater 2',
+      keyCode: 87,
+      key: 'W',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+    },
+    {
+      label: 'Heater 3',
+      keyCode: 69,
+      key: 'E',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    },
+    {
+      label: 'Chord 1',
+      keyCode: 65,
+      key: 'A',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+    },
+    {
+      label: 'Disco Oh',
+      keyCode: 83,
+      key: 'S',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    },
+    {
+      label: 'Kick N Hat',
+      keyCode: 68,
+      key: 'D',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    },
+    {
+      label: 'RP4 Kick',
+      keyCode: 90,
+      key: 'Z',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    },
+    {
+      label: 'Cev H2',
+      keyCode: 67,
+      key: 'X',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    },
+    {
+      label: 'Chord 2',
+      keyCode: 88,
+      key: 'C',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+    },
+  ];
+
+  // useEffect(()=>{
+
+  // }, [])
 
   return (
     <div className='DrumMachine'>
