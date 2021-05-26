79929495262a791f5425640a4cb878e312c57288
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-26 20:29:45 +0900

    added initial container components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import DrumMachine from './Container/DrumMachine';
+
 function App() {
   return (
     <div className='App'>
-      <h2>Hello. This is the future site of the drum machine react app</h2>
+      <DrumMachine />
     </div>
   );
 }
---
+++ src/Container/DrumMachine.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function DrumMachine() {
+  // Display Key Press Sate
+  const [display, setDisplay] = useState('Press any key');
+
+  return (
+    <div className='DrumMachine'>
+      <div>
+        <buttons>Blah</buttons>
+      </div>
+    </div>
+  );
+}
+
+export default DrumMachine;
