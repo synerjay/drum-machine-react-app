77457bd08fd4cb037e7bf392089687df49d9e5f5
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-27 20:46:52 +0900

    renamed some stuff

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function DrumPad({ label, handleClick, key, url }) {
   return (
-    <button id={label} className='drum-pad' onClick={handleClick}>
-      {key}
-      <audio id={key} className='clip' src={url}></audio>
-    </button>
+    <div className='drumPadContainer'>
+      <button id={label} className='drum-pad' onClick={handleClick}>
+        {key}
+        <audio id={key} className='clip' src={url}></audio>
+      </button>
+    </div>
   );
 }
 
