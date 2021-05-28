ca015879c35f47ee9fdfec3fcbeb669bb80410a1
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-28 21:35:27 +0900

    Added button component

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,13 +1,14 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 
 function Pad({ audioFiles, handleClick }) {
   return (
     <div className='DrumPad'>
       {audioFiles.map((item) => (
-        <button id={item.label} className='drum-pad' onClick={handleClick}>
+        <Button id={item.label} className='drum-pad' onClick={handleClick}>
           {item.key}
           <audio id={item.key} className='clip' src={item.url}></audio>
-        </button>
+        </Button>
       ))}
     </div>
   );
