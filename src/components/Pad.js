3b993f1cc3f2eff96f90cc1625bb1bea4d915858
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-29 18:57:59 +0900

    Added some styles

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -5,14 +5,15 @@ function Pad({ audioFiles, handleClick }) {
   return (
     <div className='DrumPad grid grid-cols-3 gap-4'>
       {audioFiles.map((item) => (
-        <Button
+        <button
+          color='secondary'
           key={item.label}
-          className='drum-pad'
+          className='drum-pad h-32 w-32 focus:outline-none bg-gray-300 text-gray-50 shadow-inner'
           onClick={() => handleClick(item.key)}
         >
           {item.key}
           <audio id={item.key} className='clip' src={item.url}></audio>
-        </Button>
+        </button>
       ))}
     </div>
   );
