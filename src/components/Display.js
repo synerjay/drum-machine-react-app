cf31bbfa61f120945fc652fa5cd5c88e82c86f39
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-29 18:59:47 +0900

    added margin bottom

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Display({ display }) {
   return (
     <div className='Display'>
-      <h2 className='DisplayText text-5xl flex justify-center'>{display}</h2>
+      <h2 className='DisplayText text-5xl flex justify-center mb-10'>
+        {display}
+      </h2>
     </div>
   );
 }
