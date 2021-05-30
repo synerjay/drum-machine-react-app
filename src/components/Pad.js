ece88511afaceec9e62a5cfddc4e6552b8604863
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-30 19:14:13 +0900

    Removed Material UI dependency and other unnecessary codes

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,4 +1,3 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
 
 // Reference for keypress and active button
--- src/containers/DrumMachine.js
+++ src/containers/DrumMachine.js
@@ -7,8 +7,6 @@ function DrumMachine() {
   // Display Key Press Sate
   const [display, setDisplay] = useState('Press any key');
 
-
-
   useEffect(() => {
     //ComponentWillMount
     document.addEventListener('keydown', handleKeyPress);
@@ -31,21 +29,23 @@ function DrumMachine() {
     playSound(id);
     const foundLabel = audioFiles.find((item) => item.key === id);
     setDisplay(foundLabel.label);
-    const idArray = buttonRef.current.find((element) => element.id === foundLabel.label)
-    idArray.focus()
-    setTimeout(() => idArray.blur(), 5)
-    console.log(foundLabel.keyCode)
+    const idArray = buttonRef.current.find(
+      (element) => element.id === foundLabel.label
+    );
+    idArray.focus();
+    setTimeout(() => idArray.blur(), 5);
   };
 
   const handleKeyPress = (e) => {
     const target = audioFiles.find((item) => item.keyCode === e.keyCode);
-    const idArray = buttonRef.current.find((element) => element.id === target.label)
-    idArray.focus()
-    setTimeout(() => idArray.blur(), 100)
+    const idArray = buttonRef.current.find(
+      (element) => element.id === target.label
+    );
+    idArray.focus();
+    setTimeout(() => idArray.blur(), 100);
     playSound(String.fromCharCode(e.keyCode));
     setDisplay(target.label);
   };
-
 
   return (
     <div className='DrumMachine flex flex-col justify-center w-96'>
