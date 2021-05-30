1e06cfc006a16fa6727802c7bec2d37b152c3b72
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-30 19:35:58 +0900

    Removed some remnant code from Material UI

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -3,14 +3,13 @@ import React from 'react';
 // Reference for keypress and active button
 // https://stackoverflow.com/questions/51413364/apply-button-active-styles-on-keypress-in-react
 
-// Having useRef in an array of Elements
+// For future reference: Having useRef in an array of Elements
 // https://mattclaffey.medium.com/adding-react-refs-to-an-array-of-items-96e9a12ab40c
 function Pad({ audioFiles, handleClick, buttonRef, padStyle }) {
   return (
     <div className='DrumPad grid grid-cols-3 gap-4'>
       {audioFiles.map((item) => (
         <button
-          color='secondary'
           id={item.label}
           key={item.key}
           ref={(element) => buttonRef.current.push(element)}
